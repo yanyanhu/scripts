3e23e4e363831d4338f7adff8ee62d999a6be5c3
yanyanhu <yanyanhu@cn.ibm.com>
2016-07-26 17:20:32 +0800

    Script for starting mesos agent standalone.

---
+++ docker/mesos_agent_standalone.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MASTER_IP=$1
+
+docker run -d \
+  --privileged=true \
+  --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+  --volume /usr/bin/docker:/usr/bin/docker:ro \
+  --net=host \
+  --name mesos-agent mesos-slave-ppc64:20160726 \
+  --master=$MASTER_IP:5050 --containerizers=docker,mesos \
+  --executor_registration_timeout=15mins \
+  --port=5051 --log_dir=/var/log/mesos \
+  --work_dir=/mesos --resources="ports(*):[31000-33200]"
