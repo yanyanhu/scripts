d45adcad0c64e6f8a68823e246d32d22e0b6c380
yanyanhu <yanyanhu@cn.ibm.com>
2016-07-27 17:52:32 +0800

    Revise mesos_agent_standalone.sh.

--- docker/mesos_agent_standalone.sh
+++ docker/mesos_agent_standalone.sh
@@ -2,13 +2,15 @@
 
 MASTER_IP=$1
 
-docker run -d \
+docker run -idt \
   --privileged=true \
   --volume /var/run/docker.sock:/var/run/docker.sock:ro \
   --volume /usr/bin/docker:/usr/bin/docker:ro \
   --net=host \
   --name mesos-agent mesos-slave-ppc64:20160726 \
   --master=$MASTER_IP:5050 --containerizers=docker,mesos \
+  --hostname_lookup=false \
   --executor_registration_timeout=15mins \
   --port=5051 --log_dir=/var/log/mesos \
+  --no-systemd_enable_support \
   --work_dir=/mesos --resources="ports(*):[31000-33200]"
