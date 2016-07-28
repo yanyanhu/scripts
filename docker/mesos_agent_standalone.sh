ba0e12f7daa4034b886edd665f260e7f5ac9debe
yanyanhu <yanyanhu@cn.ibm.com>
2016-07-28 10:51:18 +0800

    Revise scripts for mesos standalone.

--- docker/mesos_agent_standalone.sh
+++ docker/mesos_agent_standalone.sh
@@ -7,10 +7,10 @@ docker run -idt \
   --volume /var/run/docker.sock:/var/run/docker.sock:ro \
   --volume /usr/bin/docker:/usr/bin/docker:ro \
   --net=host \
-  --name mesos-agent mesos-slave-ppc64:20160726 \
+  --name test-mesos-agent 9.186.91.246:5000/yanyanhu/mesos-agent-gpu-ppc64le:20160726 \
   --master=$MASTER_IP:5050 --containerizers=docker,mesos \
   --hostname_lookup=false \
   --executor_registration_timeout=15mins \
   --port=5051 --log_dir=/var/log/mesos \
   --no-systemd_enable_support \
-  --work_dir=/mesos --resources="ports(*):[31000-33200]"
+  --work_dir=/mesos --resources="ports(*):[31000-32000]"
--- docker/mesos_master_standalone.sh
+++ docker/mesos_master_standalone.sh
@@ -3,5 +3,5 @@
 HOST_IP=$1
 
 docker run -d --net host \
- --name mesos-master mesos-master-ppc64:20160726 \
+ --name test-mesos-master 9.186.91.246:5000/yanyanhu/mesos-master-gpu-ppc64le:20160726 \
  --ip=$HOST_IP --work_dir=/mesos
