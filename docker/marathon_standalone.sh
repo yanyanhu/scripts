495f88acf1bcbeee6f8198e7265dceb32ce5fc04
yanyanhu <yanyanhu@cn.ibm.com>
2016-08-01 17:39:02 +0800

    Fix marathon_standalone.sh.

--- docker/marathon_standalone.sh
+++ docker/marathon_standalone.sh
@@ -5,5 +5,5 @@ ZOOKEEPER_IP=$2
 
 docker run -idt -p 8080:8080\
  --name test-marathon marathon-ppc64le-auto:0.13.0 \
- --master $MESOS_MASTER_IP:5050 --zk zk://$ZOOKEEPER_IP:2181/marathon
+ --master $MESOS_MASTER_IP:5050 --zk zk://$ZOOKEEPER_IP:2181/marathon \
  --zk_timeout 300000 --local_port_max 40000
