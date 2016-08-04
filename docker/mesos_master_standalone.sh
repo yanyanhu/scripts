28f29d1500736ec3b37c80efb7b24195c9fd0db6
yanyanhu <yanyanhu@cn.ibm.com>
2016-08-04 10:50:59 +0800

    Some changes on marathon and mesos_master script.

--- docker/mesos_master_standalone.sh
+++ docker/mesos_master_standalone.sh
@@ -2,6 +2,6 @@
 
 HOST_IP=$1
 
-docker run -d --net host \
+docker run -idt \
  --name test-mesos-master 9.186.91.246:5000/yanyanhu/mesos-master-gpu-ppc64le:20160726 \
- --ip=$HOST_IP --work_dir=/mesos
+ --ip=$HOST_IP --work_dir=/mesos --hostname_lookup=false
