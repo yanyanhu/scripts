978e301922d6ab2c2637a7573051a8563640e3e2
Yanyan Hu <yanyanhu@cn.ibm.com>
2017-01-17 14:29:40 +0800

    Accept node_ip as input param in run-k8s-node.sh

--- k8s/run-k8s-node.sh
+++ k8s/run-k8s-node.sh
@@ -4,7 +4,7 @@ export KUBE_HOME=/home/huyanyan/k8s/kubernetes/_output/local/bin/linux/amd64
 export ETCD_SERVER=9.186.107.219
 export KUBE_APISERVER=9.186.107.219
 export KUBE_DNS=9.186.107.219
-export NODE_IP=9.186.107.219
+export NODE_IP=$1
 
 echo "starting proxy..."
 ./start-k8s-proxy.sh
