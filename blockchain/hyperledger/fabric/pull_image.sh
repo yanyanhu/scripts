c87b79a626524040622fdebf8f70ffa76e899207
Yanyan <yanyanhu@sg.ibm.com>
2017-12-07 03:06:40 +0000

    Fix pull_image.sh

--- blockchain/hyperledger/fabric/pull_image.sh
+++ blockchain/hyperledger/fabric/pull_image.sh
@@ -5,7 +5,7 @@ REMOTE_TAG=x86_64-1.0.5
 LOCAL_TAG=latest
 
 dockerImagesPull() {
-  for IMAGE in peer ca orderer couchdb ccenv javaenv kafka zookeeper testenv; do
+  for IMAGE in peer ca orderer couchdb ccenv javaenv kafka zookeeper tools; do
       echo "==> PULLING FABRIC IMAGE: $IMAGE"
       echo
       docker pull hyperledger/fabric-$IMAGE:$REMOTE_TAG
