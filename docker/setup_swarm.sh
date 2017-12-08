1a0b3c44274fac001c6225e945ae2ee3a30674c8
Yanyan Hu <yanyanhu@sg.ibm.com>
2017-12-08 10:05:13 +0000

    Minor fix on docker swarm setup

--- docker/setup_swarm.sh
+++ docker/setup_swarm.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# In both Master and workers
 apt-get -y install docker.io
-echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --cluster-store=consul://9.1.34.129:8500 --cluster-advertise=eth0:2376 --label owner=org1"' >> /etc/default/docker
+export MASTERIP=9.1.34.139
+echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --cluster-store=consul://${MASTERIP}:8500 --cluster-advertise=eth0:2375 --label owner=org1"' >> /etc/default/docker
+sed -e 's/${MASTERIP}/'$MASTERIP'/g' /etc/default/docker > /etc/default/docker
 /etc/init.d/docker restart
 
 # For label based node filtering, using the following format definition in docker-compose file
@@ -16,6 +19,7 @@ docker run -d -p 8500:8500 --name=consul progrium/consul -server -bootstrap -adv
 docker run -d -p 4000:4000 swarm manage -H :4000 --replication --advertise $MASTERIP:4000 consul://$MASTERIP:8500
 
 # In Workers
+export MASTERIP=9.1.34.139
 export WORKERIP=9.1.34.140
 docker run -d swarm join --advertise=$WORKERIP:2375 consul://$MASTERIP:8500
 
@@ -24,3 +28,7 @@ docker run -d swarm join --advertise=$WORKERIP:2375 consul://$MASTERIP:8500
 
 docker -H :4000 info
 docker -H :4000 run hello-world
+
+# To use docker-compose, set DOCKER_HOST=4000 environment variable. Otherwise,
+# docker-compose will talk to default docker port rather than the one of swarm manager.
+export DOCKER_HOST=":4000"
