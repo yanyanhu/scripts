5d457d6bce8dd6cef92c47af451f2a749445be7e
Yanyan Hu <yanyanhu@sg.ibm.com>
2018-01-15 05:49:41 +0000

    Script to check disk UUID

--- blockchain/hyperledger/fabric/download_binaries.sh
+++ blockchain/hyperledger/fabric/download_binaries.sh
@@ -2,4 +2,4 @@
 
 # Reference: http://hyperledger-fabric.readthedocs.io/en/release/samples.html
 #
-curl -sSL https://goo.gl/byy2Qj | bash -s 1.0.5
+curl -sSL https://goo.gl/byy2Qj | bash -s 1.1.0-preview
---
+++ operation/disk_uuid.sh
@@ -0,0 +1,1 @@
+blkid -o full -s UUID
