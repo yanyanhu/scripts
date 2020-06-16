7db6176c1b012710de2da01ed6e3495cd274d663
yanyanhu <yanyan.hu@moht.com.sg>
2020-06-16 13:06:20 +0800

    Minor fix on aws/stack_existence_check.sh

--- aws/stack_existence_check.sh
+++ aws/stack_existence_check.sh
@@ -3,16 +3,17 @@
 STACK_NAME=$1
 RUNTIME=${2:-60} # 60 seconds timeout
 SLEEPTIME=${3:-10} # 10 seconds sleep time
-STACK_EXISTENCE=1
 
 echo "Waiting for Stack $STACK_NAME to be deleted..."
 SECONDS=0 # Reset seconds counting
+STACK_STR='"StackName": "gitlab-example"'
 while [ $SECONDS -lt $RUNTIME ]
 do
-    # Add status filter is needed, e.g. "--stack-status-filter CREATE_COMPLETE"
-    STACK_EXISTENCE=$(aws cloudformation list-stacks --region ap-southeast-1 | grep $STACK_NAME | wc -l)
-    echo "Stack eixstence status: $STACK_EXISTENCE"
-    if [ "$STACK_EXISTENCE" -eq 0 ]; then
+    STACK_AMOUNT=$(aws cloudformation list-stacks --region ap-southeast-1 | grep "$STACK_STR" | wc -l)
+    echo "Total stack amount: $STACK_AMOUNT"
+    DELETED_STACK_AMOUNT=$(aws cloudformation list-stacks --stack-status-filter DELETE_COMPLETE --region ap-southeast-1 | grep "$STACK_STR" | wc -l)
+    echo "DELETED stack amount: $DELETED_STACK_AMOUNT"
+    if [ "$STACK_AMOUNT" -eq "$DELETED_STACK_AMOUNT" ]; then
         echo "Stack has been deleted successfully."
         break
     fi
@@ -20,6 +21,7 @@ do
     sleep $SLEEPTIME
 done
 
-if [ "$STACK_EXISTENCE" -ne 0 ]; then
+if [ "$STACK_AMOUNT" -ne "$DELETED_STACK_AMOUNT" ]; then
     echo "Failed to delete Stack $STACK_NAME within $RUNTIME seconds."
 fi
+
