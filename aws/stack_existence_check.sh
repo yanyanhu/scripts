182953502e5e20289f31abf9c6e3186b6968a502
yanyanhu <yanyan.hu@moht.com.sg>
2020-06-16 12:43:30 +0800

    Minor fix on aws/stack_existence_check.sh

--- aws/stack_existence_check.sh
+++ aws/stack_existence_check.sh
@@ -9,7 +9,8 @@ echo "Waiting for Stack $STACK_NAME to be deleted..."
 SECONDS=0 # Reset seconds counting
 while [ $SECONDS -lt $RUNTIME ]
 do
-    STACK_EXISTENCE=$(aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE --region ap-southeast-1 | grep $STACK_NAME | wc -l)
+    # Add status filter is needed, e.g. "--stack-status-filter CREATE_COMPLETE"
+    STACK_EXISTENCE=$(aws cloudformation list-stacks --region ap-southeast-1 | grep $STACK_NAME | wc -l)
     echo "Stack eixstence status: $STACK_EXISTENCE"
     if [ "$STACK_EXISTENCE" -eq 0 ]; then
         echo "Stack has been deleted successfully."
@@ -22,4 +23,3 @@ done
 if [ "$STACK_EXISTENCE" -ne 0 ]; then
     echo "Failed to delete Stack $STACK_NAME within $RUNTIME seconds."
 fi
-
