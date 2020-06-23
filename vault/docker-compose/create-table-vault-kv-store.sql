43cf2c9f73e8da1c75a905e11ef6b319d92d7fde
yanyanhu <yanyan.hu@moht.com.sg>
2020-06-23 09:55:32 +0000

    Minor change on create-table-vault-kv-store.sql

--- vault/docker-compose/create-table-vault-kv-store.sql
+++ vault/docker-compose/create-table-vault-kv-store.sql
@@ -7,3 +7,11 @@ CREATE TABLE vault_kv_store (
 );
 
 CREATE INDEX parent_path_idx ON vault_kv_store (parent_path);
+
+CREATE TABLE vault_ha_locks (
+  ha_key                                      TEXT COLLATE "C" NOT NULL,
+  ha_identity                                 TEXT COLLATE "C" NOT NULL,
+  ha_value                                    TEXT COLLATE "C",
+  valid_until                                 TIMESTAMP WITH TIME ZONE NOT NULL,
+  CONSTRAINT ha_key PRIMARY KEY (ha_key)
+);
