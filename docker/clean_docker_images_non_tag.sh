351973992e91e2dd34a303a5917a09bae82930a5
yanyanhu <yanyan.hu@moht.com.sg>
2020-08-20 03:52:04 +0000

    Minor change on clean_docker_images_non_tag.sh

--- docker/clean_docker_images_non_tag.sh
+++ docker/clean_docker_images_non_tag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PROJECT_NAME=blue-audit
+PROJECT_NAME=$1
 IMAGE_LIST=`docker images | grep $PROJECT_NAME | grep -i none | awk  '{print $3}'`
 
 for image in $IMAGE_LIST
@@ -8,3 +8,6 @@ do
     echo $image
     docker rmi $image
 done
+
+# Or simply run the following cmd
+# docker rmi $(docker images -f "dangling=true" -q)
